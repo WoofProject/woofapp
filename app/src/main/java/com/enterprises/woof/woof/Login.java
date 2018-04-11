7f3e2d0b98032887abe0db989f775b504cb49b95
Marco <marco@windows.com>
2018-04-11 21:39:02 +1200

    modified interface design commit

---
+++ app/src/main/java/com/enterprises/woof/woof/Login.java
@@ -0,0 +1,27 @@
+package com.enterprises.woof.woof;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btn = (Button)findViewById(R.id.learnmore);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, MainActivity.class));
+            }
+        });
+    }
+
+
+}
