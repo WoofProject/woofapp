b632131073b5814dafe96ecb9502efc910051f52
Kyseen <kyseen.sukhdev@hotmail.com>
2018-05-03 10:12:34 +1200

    Medication

--- app/src/main/java/com/enterprises/woof/woof/Medications.java
+++ app/src/main/java/com/enterprises/woof/woof/Medications.java
@@ -2,16 +2,14 @@ package com.enterprises.woof.woof;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Medications extends AppCompatActivity {
-
-    private String medicationName;
-    private String startDate;
-    private String endDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medications);
     }
+
 }
