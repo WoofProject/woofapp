a33fc8294072db405f7391b91e971c0822e77067
Marco <marco@windows.com>
2018-05-04 23:28:36 +1200

    Marco commit

--- app/src/main/java/com/enterprises/woof/woof/Medications.java
+++ app/src/main/java/com/enterprises/woof/woof/Medications.java
@@ -2,7 +2,9 @@ package com.enterprises.woof.woof;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Medications extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class Medications extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medications);
+
+        findViewById(R.id.addMedication).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.addNewMedication)).setText("HELLO");
+            }
+        });
     }
 
 }
