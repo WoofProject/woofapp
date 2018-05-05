9115b55fb3a455b6fd70e509e7b54b3b7289cfa0
k7ameet <enterpriseswoof@gmail.com>
2018-05-05 12:02:44 +1200

    lol

--- app/src/main/java/com/enterprises/woof/woof/Maps.java
+++ app/src/main/java/com/enterprises/woof/woof/Maps.java
@@ -40,11 +40,11 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        getLocation(url);
+        //getLocation(url);
         // Add a marker in Sydney and move the camera
-        // LatLng sydney = new LatLng(-34, 151);
-        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     public void getLocation (String url) {
