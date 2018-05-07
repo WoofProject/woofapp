21989189e2a91fbc9f0fe629550eab69d2b1a7fe
Marco <marco@windows.com>
2018-05-07 23:13:33 +1200

    Marco commit

--- app/src/main/java/com/enterprises/woof/woof/Maps.java
+++ app/src/main/java/com/enterprises/woof/woof/Maps.java
@@ -47,18 +47,7 @@ public class Maps extends FragmentActivity implements OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
-//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14), 5000, null);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 14), 5000, null);
     }
 
     public void getLocation (String url) {
