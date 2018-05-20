346e16c253bf7acf2e5584be2bb9ba5ba38bcd6a
gandharparanjape <gandhar.paranjape@gmail.com>
2018-05-20 23:04:50 +1200

    Merge branch 'master' of https://github.com/WoofProject/woofapp
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/res/layout/activity_appointments.xml

--- app/src/main/java/com/enterprises/woof/woof/Locator.java
+++ app/src/main/java/com/enterprises/woof/woof/Locator.java
@@ -2,15 +2,42 @@ package com.enterprises.woof.woof;
 
 import com.google.gson.Gson;
 
+import java.io.DataOutputStream;
+import java.net.*;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class Locator {
 
-    public TrackimoDevice getDevice (String url) {
-        /*
-        get request
-         */
-        String jsonObject = "";
-        Gson gson = new Gson();
-        TrackimoDevice device = gson.fromJson(jsonObject, TrackimoDevice.class);
+    public TrackimoDevice getDevice () {
+        TrackimoDevice device = null;
+        try {
+            URL url = new URL("https://app.trackimo.com/api/v3/accounts/81361/locations/filter?limit=");
+
+
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+            String urlParameters = "";
+            connection.setRequestProperty(" ", " ");
+            connection.setRequestProperty(" ", " ");
+            connection.setRequestProperty(" ", " ");
+            connection.setRequestProperty(" ", " ");
+            connection.setRequestProperty(" ", " ");
+            connection.setRequestProperty(" ", " ");
+
+            connection.setDoOutput(true);
+            DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+
+            dStream.writeBytes(urlParameters);
+            dStream.flush();
+            dStream.close();
+
+
+            String jsonObject = "";
+            Gson gson = new Gson();
+            device = gson.fromJson(jsonObject, TrackimoDevice.class);
+        } catch (MalformedURLException e){}
         return device;
     }
+
 }
